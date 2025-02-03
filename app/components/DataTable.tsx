f5df3340448f08d8534968a82282a1a3adbf8da1
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-02-03 18:12:23 +0200

    save

--- app/components/DataTable.tsx
+++ app/components/DataTable.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { formatDate } from "@/lib/utils";
 import {
   flexRender,
   getCoreRowModel,
@@ -10,25 +11,6 @@ import { dummyData } from "../dummyData";
 import { ColumnSettings } from "./ColumnSettings";
 import { Filters } from "./Filters";
 
-// const columns = [
-//   {
-//     header: "First Name",
-//     accessorKey: "firstName",
-//   },
-//   {
-//     header: "Last Name",
-//     accessorKey: "lastName",
-//   },
-//   {
-//     header: "Email",
-//     accessorKey: "email",
-//   },
-//   {
-//     header: "Age",
-//     accessorKey: "age",
-//   },
-// ];
-
 const DataTable = () => {
   const [columnVisibility, setColumnVisibility] = useState<
     Record<string, boolean>
@@ -36,7 +18,9 @@ const DataTable = () => {
     firstName: true,
     lastName: true,
     email: true,
-    age: false,
+    age: true,
+    startDate: true,
+    endDate: true,
   });
 
   const table = useReactTable({
@@ -73,7 +57,6 @@ const DataTable = () => {
                     <div className="tooltip">
                       <span className="tooltiptext">
                         <strong>Name:</strong> {column?.header} <br />
-                        {/* <strong>Data Type:</strong> {column?.dataType} <br /> */}
                         <strong>Description:</strong> {column?.description}
                       </span>
                     </div>
@@ -87,11 +70,25 @@ const DataTable = () => {
         <tbody>
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="p-2 border border-gray-300">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+              {row.getVisibleCells().map((cell) => {
+                // Check if the column data type is Date and format accordingly
+                const column = dummyColumns.find(
+                  (col) => col.accessorKey === cell.column.id
+                );
+                const cellValue = cell.getValue();
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                let formattedCellValue: any = cellValue;
+
+                if (column?.dataType === "date" && cellValue instanceof Date) {
+                  formattedCellValue = formatDate(cellValue);
+                }
+
+                return (
+                  <td key={cell.id} className="p-2 border border-gray-300">
+                    {flexRender(formattedCellValue, cell.getContext())}
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
